338c52e1b823d587831b5e2455f20b1e32d7bb78
OmiKat <ompraveer@gmail.com>
2026-04-11 05:08:24 +0530

    .

---
+++ src/main/java/com/example/RealTimeEventStreaming/RealTimeEventStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.example.RealTimeEventStreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeEventStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeEventStreamingApplication.class, args);
+	}
+
+}
