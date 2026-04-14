19369d51980a588cdab67213897222c20030ee30
OmiKat <ompraveer@gmail.com>
2026-04-15 05:22:20 +0530

    .

---
+++ src/main/java/com/example/RealTimeEventStreaming/controller/EventController.java
@@ -0,0 +1,25 @@
+package com.example.RealTimeEventStreaming.controller;
+
+import com.example.RealTimeEventStreaming.domain.Event;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EventController {
+
+    private final KafkaTemplate<String,Event> kafkaTemplate;
+
+    @PostMapping("/users")
+    public ResponseEntity<String> sendToKafka(@RequestBody Event event){
+
+
+        return new ResponseEntity<>("Successfully sent" , HttpStatus.OK);
+    }
+
+}
