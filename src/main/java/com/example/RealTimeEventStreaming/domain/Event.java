9fd0e2f4f8971cb1425f9eb0c029088376b9a509
OmiKat <ompraveer@gmail.com>
2026-04-15 05:17:36 +0530

    general setup

---
+++ src/main/java/com/example/RealTimeEventStreaming/domain/Event.java
@@ -0,0 +1,35 @@
+package com.example.RealTimeEventStreaming.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "events")
+public class Event {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String playerId;
+
+    private String sessionId;
+
+    private EventType eventType;
+
+    private Instant timeStamp;
+
+    private Map<String,Object> metadata;
+
+}
---
+++ src/main/java/com/example/RealTimeEventStreaming/domain/EventType.java
@@ -0,0 +1,11 @@
+package com.example.RealTimeEventStreaming.domain;
+
+public enum EventType {
+    PLAYER_KILL,
+    PLAYER_DEATH,
+    LEVEL_UP,
+    ACHIEVEMENT_UNLOCKED,
+    ITEM_PURCHASED,
+    SESSION_START,
+    SESSION_END
+}
