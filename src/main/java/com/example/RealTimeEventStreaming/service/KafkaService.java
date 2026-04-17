240c2de4550052efed04d7346be16b410d445ee8
OmiKat <ompraveer@gmail.com>
2026-04-17 18:34:02 +0530

    chage

---
+++ src/main/java/com/example/RealTimeEventStreaming/service/KafkaService.java
@@ -0,0 +1,30 @@
+package com.example.RealTimeEventStreaming.service;
+
+import com.example.RealTimeEventStreaming.domain.Event;
+import com.example.RealTimeEventStreaming.producer.EventProducer;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaService {
+
+    private final EventProducer eventProducer;
+
+    public void publishEvent(Event event){
+        if(event.getTimeStamp()==null){
+            event.setTimeStamp(Instant.now());
+        }
+        if(event.getId() == null){
+            event.setId(UUID.randomUUID().toString());
+        }
+        eventProducer.SendEvent(event);
+    }
+
+}
